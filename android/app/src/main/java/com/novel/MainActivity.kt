3e6ceecfab03dc8f2f901c5be58f99d4a89638ab
VaIOReTto1 <1302140648@qq.com>
2025-05-24 15:45:27 +0800

    test

--- android/app/src/main/java/com/novel/MainActivity.kt
+++ android/app/src/main/java/com/novel/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "Novels"
+  override fun getMainComponentName(): String = "Novel"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
