89dd936afd51ba5300d3f73a558769bd0679feb1
VaIOReTto1 <1302140648@qq.com>
2025-05-30 16:14:36 +0800

    feat(book): 添加书籍封面组件- 实现了一个展示书籍封面的组件 BookCoverSection - 组件使用了 NovelImageView 来显示书籍图片 - 封面图片应用了圆角裁剪和背景色 - 组件的位置和尺寸根据屏幕宽度动态调整

---
+++ android/app/src/main/java/com/novel/page/book/components/BookCoverSection.kt
@@ -0,0 +1,39 @@
+package com.novel.page.book.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import com.novel.page.book.viewmodel.BookDetailUiState
+import com.novel.page.component.NovelImageView
+import com.novel.ui.theme.NovelColors
+import com.novel.utils.wdp
+
+@Composable
+fun BookCoverSection(bookInfo: BookDetailUiState.BookInfo?) {
+    if (bookInfo == null) return
+
+    Box(
+        modifier = Modifier.padding(top = 75.wdp)
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        NovelImageView(
+            imageUrl = bookInfo.picUrl,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .width(125.wdp)
+                .height(190.wdp)
+                .clip(RoundedCornerShape(5.wdp))
+                .background(NovelColors.NovelMainLight),
+        )
+    }
+}
